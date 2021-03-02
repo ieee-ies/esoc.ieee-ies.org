2e9240438afe1b6c137c892c583a537dffa33916
Tarek Ould-Bachir <65610654+ouldbachir-ieee@users.noreply.github.com>
2021-03-02 13:32:45 -0500

    Update members.sh

--- bin/members.sh
+++ bin/members.sh
@@ -24,7 +24,7 @@ curl -s -L -R $MEMBERSURL -o "$FILE.tsv"
 
 if [ -s $FILE".tsv" ]; then
   cat "../template_header.html" >$FILE".html"
-  echo "<div class=\"title\">Members!</div> <div class=\"subcont\">" >>$FILE".html"
+  echo "<div class=\"title\">Members</div> <div class=\"subcont\">" >>$FILE".html"
 
   tail -n +3 $FILE".tsv" |
     sed -e "/^[[:space:]]*$/d" |
@@ -32,13 +32,13 @@ if [ -s $FILE".tsv" ]; then
     awk 'BEGIN { FS = "\t" } ;
 {if (length($8) < 9)
   {
-  {print "<div class=\"member\"><div class=\"image\"><img align=\"left\" src=\"https://scholar.google.com/citations/images/avatar_scholar_128.png\" width=\"60\" height=\"60\" alt=\"" $2 "\" /></div>"}
+  {print "<div class=\"member\"><div class=\"image\"><img align=\"left\" src=\"https://scholar.google.com/citations/images/avatar_scholar_128.png\" width=\"60\" alt=\"" $2 "\" /></div>"}
   {print "<div class=\"member_data\">"}
   {print "<p><b>" $2 "</b><br />"}
   }
 else
   {
-  {print "<div class=\"member\"><div class=\"image\"><img align=\"left\" src=\"https://scholar.google.com/citations?view_op=medium_photo&user=" $8 "\" width=\"60\" height=\"60\" alt=\"" $2 "\" /></div>"}
+  {print "<div class=\"member\"><div class=\"image\"><img align=\"left\" src=\"https://scholar.google.com/citations?view_op=medium_photo&user=" $8 "\" width=\"60\" alt=\"" $2 "\" /></div>"}
   {print "<div class=\"member_data\">"}
   {print "<p><b><a href=\"https://scholar.google.com/citations?user=" $8 "\">" $2 "</a></b><br />"}
   }
